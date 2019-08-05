47f0e8a68e60df64116e82cc14c4944308e88c6d
xiaozhangi <2111800200@qq.com>
2019-08-05 17:47:11 +0800

    测试8.4 17:00

--- src/main/java/com/telecomyt/item/web/mapper/TaskMapper.java
+++ src/main/java/com/telecomyt/item/web/mapper/TaskMapper.java
@@ -47,11 +47,11 @@ public interface TaskMapper {
      * @param taskCardId
      * @return
      */
-   List<Task> queryMyTaskById(String taskCardId,Integer groupId);
+   List<Task> queryMyTaskById(@Param("taskCardId") String taskCardId,@Param("groupId")Integer groupId);
 
 
-   List<Task> queryNewTask(String taskCardId,Integer groupId);
-   List<Task>queryOtherTask(String taskCardId,Integer groupId);
+   List<Task> queryNewTask(@Param("taskCardId") String taskCardId,@Param("groupId")Integer groupId);
+   List<Task>queryOtherTask(@Param("taskCardId") String taskCardId,@Param("groupId")Integer groupId);
     /**
      *查询任务日志
      * @param groupId
