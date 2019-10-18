d6d3f0eda7f9426d2d4af9216236425e3a783eb3
quboka <1362420246@qq.com>
2019-10-18 11:32:30 +0800

    注释

--- src/main/java/com/qbk/config/RedisConfig.java
+++ src/main/java/com/qbk/config/RedisConfig.java
@@ -17,6 +17,9 @@ public class RedisConfig {
      * springboot2.x 使用LettuceConnectionFactory 代替 RedisConnectionFactory
      * application.yml配置基本信息后,springboot2.x  RedisAutoConfiguration能够自动装配
      * LettuceConnectionFactory 和 RedisConnectionFactory 及其 RedisTemplate
+     *
+     * RedisConnectionFactory是 接口 。Redis连接的线程安全工厂。
+     * 实现类有：JedisConnectionFactory 、LettuceConnectionFactory
      */
     @Bean(name = "redisTemplate")
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
